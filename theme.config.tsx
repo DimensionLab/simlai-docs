be68d40ec87c8ee0d7efc140f4e9f1bd29c54fd5
Michal Takáč <takacmichal92@gmail.com>
2023-07-04 21:15:15 +0200

    feat: use new logo in header

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,7 +3,7 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <Image src="/img/simlai-logo.png" width={120} height={20} alt="" />,
+  logo: <Image src="/img/siml-logo-white-gradient.png" width={120} height={20} alt="" />,
   project: {
     link: "https://github.com/DimensionLab/simlai-docs",
   },
