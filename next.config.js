2d4aa7f5feb4f57fe0a9bbc957d632b3c2999a8c
Michal Takáč <takacmichal92@gmail.com>
2023-07-04 21:34:20 +0200

    fix: correct title and footer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  titleSuffix: ' - Siml.ai Docs',
   latex: true,
-  staticImage: true
+  staticImage: true,
+  footerText: `© ${new Date().getFullYear()} DimensionLab s.r.o., creators of Siml.ai`,
 })
 
 module.exports = withNextra()
